48f5a5f8c8ad6a80d478376977a9f2b53669e4c3
JTeofilski <teofilskijovana@gmail.com>
2022-04-11 16:39:43 +0200

    init microservices

---
+++ microservices/followers-service/followers-service/src/main/java/com/example/followersservice/FollowersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.followersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowersServiceApplication.class, args);
+	}
+
+}
